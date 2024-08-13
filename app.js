42213d4eff9b5e791280ca915a01524d191727f2
TrainerChris12 <grantchristopher109@gmail.com>
2024-08-12 20:28:02 -0400

    Finished adding the content for the landing page and the grid for the mobile design. Tried dabbling in the implementation for the API request to chatGPT/OpenAI for the generation of the user's story. Not much of it was making sense so commenting that out for now, and will worry about it when I learn more.

--- app.js
+++ app.js
@@ -10,3 +10,19 @@ hamburgerIcon.addEventListener('click', () => {
     }
 });
 
+
+// //Handling StoryForm Submission
+// document.getElementBYId('storyForm').addEventListener('submit', async (event) => {
+//     event.preventDefault();
+//     const prompt = document.getElementById('storyPrompt').value;
+//     const repsonse = await fetch('https://api.openai.com/v1/chat/completions', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({prompt})
+//     });
+//     const data = await repsonse.json();
+//     document.getElementById('currentStory').textContent = data.story;
+// });
+
